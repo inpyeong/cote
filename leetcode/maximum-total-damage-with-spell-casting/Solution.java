5f7eea713e726152975ba432d0f40997ce72228a
inpyeong <jinipyung@gmail.com>
2025-10-11 20:28:07 +0900

    Solve '3186. Maximum Total Damage With Spell Casting'

---
+++ leetcode/maximum-total-damage-with-spell-casting/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long maximumTotalDamage(int[] power) {
+        Map<Integer, Long> freq = new HashMap<>();
+        for (int p : power) {
+            freq.put(p, freq.getOrDefault(p, 0L) + 1);
+        }
+        List<Integer> uniqueDamages = new ArrayList<>(freq.keySet());
+        Collections.sort(uniqueDamages);
+
+        int m = uniqueDamages.size();
+        if (m == 0) return 0;
+
+        long[] dp = new long[m];
+        // dp[i] = maximum damage we can get considering unique damages [0...i]
+
+        dp[0] = (long) uniqueDamages.get(0) * freq.get(uniqueDamages.get(0));
+        for (int i = 1; i < m; ++i) {
+            int curr = uniqueDamages.get(i);
+            long takeCurr = (long) curr * freq.get(curr);
+            long skipCurr = dp[i - 1];
+
+            int j = i - 1;
+            while (j >= 0 && uniqueDamages.get(j) >= curr - 2) {
+                j--;
+            }
+
+            if (j >= 0) {
+                takeCurr += dp[j];
+            }
+            dp[i] = Math.max(takeCurr, skipCurr);
+        }
+
+        return dp[m - 1];
+    }
+}
