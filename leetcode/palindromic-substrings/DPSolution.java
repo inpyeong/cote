c533425fcd888c8310f773d398e4cf5e67233d64
inpyeong <jinipyung@gmail.com>
2025-11-09 22:17:20 +0900

    Solve '647. Palindromic Substrings'

---
+++ leetcode/palindromic-substrings/DPSolution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int len = 1; len <= n; ++len) {
+            for (int i = 0; i + len - 1 < n; ++i) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && (len <= 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
